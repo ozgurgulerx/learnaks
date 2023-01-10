307430e9b81ff87b668bd4caaeef5f242d1772bf
OZGUR GULER <97877847+ozgurgulerx@users.noreply.github.com>
2023-01-10 08:47:59 +0300

    Update providers.tf

--- 01-Launch AKS Cluster with Terraform/terraform-files/providers.tf
+++ 01-Launch AKS Cluster with Terraform/terraform-files/providers.tf
@@ -16,10 +16,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = "fd4d699e-3989-4c6b-a710-6a20789454eb"
-  tenant_id         = "23b4b90a-b2c2-4ef9-9888-5af93b241573"
-  client_id         = "35df0dc5-1067-40bc-8d55-bc64ef7dd209"
-  client_secret     = "DuW8Q~qt9VPR~y6ClcPgQVGzGep1qtFOrd3SybHL"
+  subscription_id   = "subscription ID"
+  tenant_id         = "tenant ID"
+  client_id         = "App ID"
+  client_secret     = "XXXXXX"
 
 
 }
